ed75587a44ed82453bd7eb179606f17dcd05b35a
76Falcone <falcone0407@gmail.com>
2026-03-26 12:48:41 -0300

    Adicionando atributos classe usuario

--- src/java/model/usuario.java
+++ src/java/model/usuario.java
@@ -2,4 +2,13 @@ package model;
 
 public class usuario {
     
+    // Atributos
+    private int idUsuario;
+    private String nomeUsuario;
+    private String cpfUsuario;
+    private String cnhUsuario;
+    private String emailUsuario;
+    private String senhaUsuario;
+    private String celularUsuario;
+    
 }
