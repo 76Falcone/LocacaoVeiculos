01c08ac2604610f4cdf89c836fb2dfda52c8af5a
76Falcone <falcone0407@gmail.com>
2026-03-26 13:21:49 -0300

    Adicionando atributos classe usuarioBuild

---
+++ src/java/model/usuarioBuilder.java
@@ -0,0 +1,17 @@
+package model;
+
+/**
+ *
+ * @author 76Falcone
+ */
+public class usuarioBuilder {
+    
+    // Atributos
+    private int idUsuario;
+    private String nomeUsuario;
+    private String cpfUsuario;
+    private String cnhUsuario;
+    private String emailUsuario;
+    private String senhaUsuario;
+    private String celularUsuario;
+}
