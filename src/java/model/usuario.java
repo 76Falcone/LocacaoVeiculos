f1bc20e5c84e5d2f51597fbd60ea635f5565c382
76Falcone <falcone0407@gmail.com>
2026-03-26 12:49:54 -0300

    Adicionando Getters e Setters classe usuario

--- src/java/model/usuario.java
+++ src/java/model/usuario.java
@@ -25,4 +25,62 @@ public class usuario {
         this.celularUsuario = celularUsuario;
     }
     
+    // Getters e Setters
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getCpfUsuario() {
+        return cpfUsuario;
+    }
+
+    public void setCpfUsuario(String cpfUsuario) {
+        this.cpfUsuario = cpfUsuario;
+    }
+
+    public String getCnhUsuario() {
+        return cnhUsuario;
+    }
+
+    public void setCnhUsuario(String cnhUsuario) {
+        this.cnhUsuario = cnhUsuario;
+    }
+
+    public String getEmailUsuario() {
+        return emailUsuario;
+    }
+
+    public void setEmailUsuario(String emailUsuario) {
+        this.emailUsuario = emailUsuario;
+    }
+
+    public String getSenhaUsuario() {
+        return senhaUsuario;
+    }
+
+    public void setSenhaUsuario(String senhaUsuario) {
+        this.senhaUsuario = senhaUsuario;
+    }
+
+    public String getCelularUsuario() {
+        return celularUsuario;
+    }
+
+    public void setCelularUsuario(String celularUsuario) {
+        this.celularUsuario = celularUsuario;
+    }
+    
 }
