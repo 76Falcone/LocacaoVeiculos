c4e081707106d446a3ba1ac8640abd2bf5669b48
76Falcone <falcone0407@gmail.com>
2026-03-27 08:52:40 -0300

    Criando atributos dentro da classe veiculo

--- src/java/model/veiculo.java
+++ src/java/model/veiculo.java
@@ -2,4 +2,14 @@ package model;
 
 public class veiculo {
     
+    // Atributos
+    private int idVeiculo;
+    private String placaVeiculo;
+    private String modeloVeiculo;
+    private String corVeiculo;
+    private double valorDiaria;
+    private String funcionalidadeVeiculo;
+    private boolean disponibilidade;
+    private boolean arCondicionadoVeiculo;
+    private String tipoCambio;
 }
