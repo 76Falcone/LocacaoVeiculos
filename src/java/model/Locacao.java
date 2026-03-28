6b0fcdbf54e6230c5ba4472004592ea70bd8aadd
76Falcone <falcone0407@gmail.com>
2026-03-28 16:36:17 -0300

    Adicionando atributos na classe model Locacao

--- src/java/model/Locacao.java
+++ src/java/model/Locacao.java
@@ -1,9 +1,21 @@
 package model;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author 76Falcone
  */
 public class Locacao {
+    
+    // Atributos
+    private int idLocacao;
+    private Usuario usuario;       
+    private Veiculo veiculo;     
+    private int qtdDias;
+    private double seguroLocacao;
+    private double valorTotal;
+    private LocalDate dataRetirada; 
+    private LocalDate dataEntrega;
     
 }
