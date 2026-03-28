1cb699cd280fcde97aef5bd5d2ce6d4e4526a822
76Falcone <falcone0407@gmail.com>
2026-03-28 14:38:48 -0300

    Adicionando metodos do builder no VeiculoBuilder

--- src/java/model/VeiculoBuilder.java
+++ src/java/model/VeiculoBuilder.java
@@ -20,4 +20,57 @@ public class VeiculoBuilder {
     // Construtor vazio
     public VeiculoBuilder() {
     }
+    
+    // Métodos "set" que retornam o próprio Builder
+    public VeiculoBuilder comIdVeiculo(int idVeiculo) {
+        this.idVeiculo = idVeiculo;
+        return this;
+    }
+    
+    public VeiculoBuilder comPlacaVeiculo (String placaVeiculo) {
+        this.placaVeiculo = placaVeiculo;
+        return this;
+    }
+    
+    public VeiculoBuilder comModeloVeiculo (String modeloVeiculo) {
+        this.modeloVeiculo = modeloVeiculo;
+        return this;
+    }
+    
+    public VeiculoBuilder comCorVeiculo (String corVeiculo) {
+        this.corVeiculo = corVeiculo;
+        return this;
+    }
+    
+    public VeiculoBuilder comValorDiaria (double valorDiaria) {
+        this.valorDiaria = valorDiaria;
+        return this;
+    }
+    
+    public VeiculoBuilder comFuncionalidadeVeiculo(String funcionalidadeVeiculo) {
+        this.funcionalidadeVeiculo = funcionalidadeVeiculo;
+        return this;
+    }
+    
+    public VeiculoBuilder comDisponibilidade (boolean disponibilidade) {
+        this.disponibilidade = disponibilidade;
+        return this;
+    }
+    
+    public VeiculoBuilder comArCondicionadoVeiculo (boolean arCondicionadoVeiculo) {
+        this.arCondicionadoVeiculo = arCondicionadoVeiculo;
+        return this;
+    }
+    
+    public VeiculoBuilder comTipoCambio (String tipoCambio) {
+        this.tipoCambio = tipoCambio;
+        return this;
+    }
+    
+    // Metodo builder
+    public Veiculo build() {
+    return new Veiculo(idVeiculo, placaVeiculo, modeloVeiculo, corVeiculo, 
+                       valorDiaria, funcionalidadeVeiculo, disponibilidade, 
+                       arCondicionadoVeiculo, tipoCambio);
+    }
 }
