88304a833f4332aa4f741cf94e4d74b525f85b32
76Falcone <falcone0407@gmail.com>
2026-03-29 19:04:09 -0300

    Adicionando métodos de CRUD na interface IVeiculoDAO

--- src/java/dao/IVeiculoDAO.java
+++ src/java/dao/IVeiculoDAO.java
@@ -1,9 +1,18 @@
 package dao;
 
+import model.Veiculo;
+import java.util.List;
+
 /**
  *
  * @author 76Falcone
  */
 public interface IVeiculoDAO {
     
+    // Metodos para o VeiculoDAO
+    void cadastrarVeiculo(Veiculo v);
+    void deletarVeiculo(int id);
+    void atualizarVeiculo(Veiculo v);
+    Veiculo visualizarVeiculoByID(int id);
+    List<Veiculo> visualizarTodosVeiculos();
 }
