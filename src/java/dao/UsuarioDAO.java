9e47874f3854ea375d5f83418d6ae2950293c110
76Falcone <falcone0407@gmail.com>
2026-04-11 15:51:31 -0300

    Adicionando metodo atualizarUsuario no usuarioDAO

--- src/java/dao/UsuarioDAO.java
+++ src/java/dao/UsuarioDAO.java
@@ -28,11 +28,27 @@ public class UsuarioDAO implements IUsuarioDAO{
     }
     
     // Deletar usuario
+    @Override
     public void deletarUsuario(Usuario u)throws ClassNotFoundException, SQLException {
         Connection con = FabricaConexao.getConexao();
         PreparedStatement comando = con.prepareStatement("delete from usuario where id = ?");
         comando.setInt(1, u.getIdUsuario());
         comando.execute();
         con.close();
+    }
+
+    // Atualizar usuario
+    public void atualizarUsuario(Usuario u)throws ClassNotFoundException, SQLException {
+        Connection con = FabricaConexao.getConexao();
+        PreparedStatement comando = con.prepareStatement("update usuario set nomeUsuario = ?, cpfUsuario = ?, cnhUsuario = ?, emailUsuario = ?, senhaUsuario = ?, celularUsuario = ? where id = ?");
+        comando.setString(1, u.getNomeUsuario());
+        comando.setString(2, u.getCpfUsuario());
+        comando.setString(3, u.getCnhUsuario());
+        comando.setString(4, u.getEmailUsuario());
+        comando.setString(5, u.getSenhaUsuario());
+        comando.setString(6, u.getCelularUsuario());
+        comando.setInt(7, u.getIdUsuario());
+        comando.execute();
+        con.close(); 
     }
 }
