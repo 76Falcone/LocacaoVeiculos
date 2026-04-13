b66508e4900caedd725de7e7f9970177cd5ccd67
76Falcone <falcone0407@gmail.com>
2026-04-12 22:06:12 -0300

    Ajuste no metodo vizualizarTodos do UsuarioDAO

--- src/java/dao/UsuarioDAO.java
+++ src/java/dao/UsuarioDAO.java
@@ -14,13 +14,14 @@ import util.FabricaConexao;
  *
  * @author 76Falcone
  */
-public class UsuarioDAO implements IUsuarioDAO{
-    
+public class UsuarioDAO implements IUsuarioDAO {
+
     // Cadastrar usuario
     @Override
-    public void cadastrarUsuario(Usuario u)throws ClassNotFoundException, SQLException {
+    public void cadastrarUsuario(Usuario u) throws ClassNotFoundException, SQLException {
         Connection con = FabricaConexao.getConexao();
-        PreparedStatement comando = con.prepareStatement("insert into usuarios (nomeUsuario, cpfUsuario, cnhUsuario, emailUsuario, senhaUsuario, celularUsuario) value (?, ?, ?, ?, ?, ?)");
+        PreparedStatement comando = con.prepareStatement(
+                "insert into usuarios (nomeUsuario, cpfUsuario, cnhUsuario, emailUsuario, senhaUsuario, celularUsuario) value (?, ?, ?, ?, ?, ?)");
         comando.setString(1, u.getNomeUsuario());
         comando.setString(2, u.getCpfUsuario());
         comando.setString(3, u.getCnhUsuario());
@@ -30,10 +31,10 @@ public class UsuarioDAO implements IUsuarioDAO{
         comando.execute();
         con.close();
     }
-    
+
     // Deletar usuario
     @Override
-    public void deletarUsuario(Usuario u)throws ClassNotFoundException, SQLException {
+    public void deletarUsuario(Usuario u) throws ClassNotFoundException, SQLException {
         Connection con = FabricaConexao.getConexao();
         PreparedStatement comando = con.prepareStatement("delete from usuario where id = ?");
         comando.setInt(1, u.getIdUsuario());
@@ -43,9 +44,10 @@ public class UsuarioDAO implements IUsuarioDAO{
 
     // Atualizar usuario
     @Override
-    public void atualizarUsuario(Usuario u)throws ClassNotFoundException, SQLException {
+    public void atualizarUsuario(Usuario u) throws ClassNotFoundException, SQLException {
         Connection con = FabricaConexao.getConexao();
-        PreparedStatement comando = con.prepareStatement("update usuario set nomeUsuario = ?, cpfUsuario = ?, cnhUsuario = ?, emailUsuario = ?, senhaUsuario = ?, celularUsuario = ? where id = ?");
+        PreparedStatement comando = con.prepareStatement(
+                "update usuario set nomeUsuario = ?, cpfUsuario = ?, cnhUsuario = ?, emailUsuario = ?, senhaUsuario = ?, celularUsuario = ? where id = ?");
         comando.setString(1, u.getNomeUsuario());
         comando.setString(2, u.getCpfUsuario());
         comando.setString(3, u.getCnhUsuario());
@@ -54,12 +56,12 @@ public class UsuarioDAO implements IUsuarioDAO{
         comando.setString(6, u.getCelularUsuario());
         comando.setInt(7, u.getIdUsuario());
         comando.execute();
-        con.close(); 
+        con.close();
     }
 
     // Buscar por ID
     @Override
-    public Usuario visualizarUsuarioByID(Usuario u)throws ClassNotFoundException, SQLException{
+    public Usuario visualizarUsuarioByID(Usuario u) throws ClassNotFoundException, SQLException {
         Connection con = FabricaConexao.getConexao();
         PreparedStatement comando = con.prepareStatement("select * from usuario where id = ?");
         comando.setInt(1, u.getIdUsuario());
@@ -81,7 +83,7 @@ public class UsuarioDAO implements IUsuarioDAO{
 
     // Metodo de buscar todos
     @Override
-    public List<Usuario> visualizarTodosUsuarios()throws ClassNotFoundException, SQLException{
+    public List<Usuario> visualizarTodosUsuarios() throws ClassNotFoundException, SQLException {
         Connection con = FabricaConexao.getConexao();
         PreparedStatement comando = con.prepareStatement("select * from usuario");
         ResultSet rs = comando.executeQuery();
@@ -95,10 +97,11 @@ public class UsuarioDAO implements IUsuarioDAO{
             user.setCnhUsuario(rs.getString("cnhUsuario"));
             user.setEmailUsuario(rs.getString("emailUsuario"));
             user.setSenhaUsuario(rs.getString("senhaUsuario"));
-            user.setCelularUsuario(rs.getString("celularUsuario"));           
+            user.setCelularUsuario(rs.getString("celularUsuario"));
+            listaUsuario.add(user);
         }
-        listaUsuario.add(user);
+        con.close();
         return listaUsuario;
-    } 
-    
+    }
+
 }
