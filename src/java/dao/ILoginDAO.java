292cb5c8e511eb8f2c71569acebdd6c39b6d484c
76Falcone <falcone0407@gmail.com>
2026-04-13 22:44:49 -0300

    Alteração de void para boolean ILoginDAO

--- src/java/dao/ILoginDAO.java
+++ src/java/dao/ILoginDAO.java
@@ -7,5 +7,5 @@ import java.sql.SQLException;
  * @author 76Falcone
  */
 public interface ILoginDAO {
-    void validarLogin(String email, String senha) throws ClassNotFoundException, SQLException;
+    boolean validarLogin(String email, String senha) throws ClassNotFoundException, SQLException;
 }
