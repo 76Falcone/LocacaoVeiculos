da84fc9b6fe00df988bd8d3232b9f9c0d448b1b2
76Falcone <falcone0407@gmail.com>
2026-04-13 23:21:16 -0300

    Adicionando metodo atualizarLocacao

--- src/java/dao/LocacaoDAO.java
+++ src/java/dao/LocacaoDAO.java
@@ -56,6 +56,26 @@ public class LocacaoDAO implements ILocacaoDAO {
     // Atualizar Locacao
     @Override
     public void atualizarLocacao(Locacao l) throws ClassNotFoundException, SQLException {
+        Connection con = FabricaConexao.getConexao();
+        PreparedStatement comando = con.prepareStatement(
+                "update locacao set id_usuario = ?, id_veiculo = ?, qtdDias = ?, seguro = ?, localRetirada = ?, valorTotal = ?, data_retirada = ?, data_entrega = ? where id = ?");
+        comando.setInt(1, l.getUsuario().getIdUsuario());
+        comando.setInt(2, l.getVeiculo().getIdVeiculo());
+        comando.setInt(3, l.getQtdDias());
+        comando.setDouble(4, l.getSeguroLocacao());
+        comando.setString(5, l.getLocalRetirada());
+        comando.setDouble(6, l.getValorTotal());
+        comando.setDate(7, Date.valueOf(l.getDataRetirada()));
+
+        if (l.getDataEntrega() != null) {
+            comando.setDate(8, Date.valueOf(l.getDataEntrega()));
+        } else {
+            comando.setNull(8, java.sql.Types.DATE);
+        }
+
+        comando.setInt(9, l.getIdLocacao());
+        comando.execute();
+        con.close();
     }
 
     // Buscar por ID
