7193a42d413054435bf4516fbc500aadbbc537c4
76Falcone <falcone0407@gmail.com>
2026-04-13 22:54:30 -0300

    Adicionando corpo no metodo validarLogin

--- src/java/dao/LoginDAO.java
+++ src/java/dao/LoginDAO.java
@@ -1,5 +1,12 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import util.FabricaConexao;
+
 /**
  *
  * @author 76Falcone
@@ -7,4 +14,20 @@ package dao;
 
 public class LoginDAO implements ILoginDAO {
 
+    @Override
+    public boolean validarLogin(String email, String senha) throws ClassNotFoundException, SQLException {
+        Connection con = FabricaConexao.getConexao();
+        PreparedStatement comando = con.prepareStatement("SELECT * FROM usuarios WHERE email = ? AND senha = ?");
+        comando.setString(1, email);
+        comando.setString(2, senha);
+        ResultSet resultado = comando.executeQuery();
+        boolean acessoLiberado = false;
+        if (resultado.next()) {
+            acessoLiberado = true;
+        } else {
+            acessoLiberado = false;
+        }
+        con.close();
+        return acessoLiberado;
+    }
 }
