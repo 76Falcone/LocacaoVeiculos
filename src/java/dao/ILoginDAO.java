bad815104c84eab7845e1ac82cf63718b3a55676
76Falcone <falcone0407@gmail.com>
2026-04-13 22:39:52 -0300

    Adicionando o metodo validarLogin dentro do ILoginDAO

--- src/java/dao/ILoginDAO.java
+++ src/java/dao/ILoginDAO.java
@@ -1,10 +1,11 @@
 package dao;
 
+import java.sql.SQLException;
+
 /**
  *
  * @author 76Falcone
  */
-
 public interface ILoginDAO {
-
+    void validarLogin(String email, String senha) throws ClassNotFoundException, SQLException;
 }
