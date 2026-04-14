a88176e2bcc3207604063151aafbefc6cd8d2fbb
76Falcone <falcone0407@gmail.com>
2026-04-13 23:16:43 -0300

    Adicionando metodos para o ILocacaoDAo

--- src/java/dao/ILocacaoDAO.java
+++ src/java/dao/ILocacaoDAO.java
@@ -9,4 +9,13 @@ import model.Locacao;
  * @author 76Falcone
  */
 public interface ILocacaoDAO {
+    void cadastrarLocacao(Locacao l) throws ClassNotFoundException, SQLException;
+
+    void deletarLocacao(Locacao l) throws ClassNotFoundException, SQLException;
+
+    void atualizarLocacao(Locacao l) throws ClassNotFoundException, SQLException;
+
+    Locacao visualizarLocacaoByID(Locacao l) throws ClassNotFoundException, SQLException;
+
+    List<Locacao> visualizarTodasLocacoes() throws ClassNotFoundException, SQLException;
 }
